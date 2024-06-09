befd29ec04bdd8fb57f17d676321f2ea0d958914
Rohit Pandey <iit2022019@iiita.ac.in>
2024-06-09 06:09:58 +0530

    updates done

--- src/pages/MySubmission.jsx
+++ src/pages/MySubmission.jsx
@@ -1,9 +1,13 @@
-import { Card } from '@mui/material'
 import React from 'react'
+import DetailedCard from '../components/DetailedCard'
+import Card from '../components/Card'
 
 const MySubmission = () => {
   return (
-    <Card />
+    <>
+        <Card/>
+        <DetailedCard name={'Rohit'}  enrollmentno ={'IIT2022019'} batch={'2022'} mobile ={'9779797979'} email={'hi@gmail.com'} aadhar={'645798464654'} idea={'Nothing'} marketsize={'Small'} verified={'No'} />
+    </>
   )
 }
 
