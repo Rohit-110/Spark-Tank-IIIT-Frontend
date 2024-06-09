14fa5b3a684d77bb3f22c295f4be0d9c83541780
Rohan Pandey <10dspchemistry@gmail.com>
2024-06-09 16:51:57 +0530

    updatesdone

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import {BrowserRouter as Router, Routes , Route} from 'react-router-dom';
+import Login from './pages/Login.jsx';
 import Payment from './pages/Payment.jsx'
 import PaymentSuccess from './pages/PaymentSuccess.jsx';
 import Form from './pages/Form.jsx';
 import MySubmission from './pages/MySubmission.jsx';
+import Signup from './pages/Signup.jsx';
 function App() {
 
   return (
     <>
       <Router>
         <Routes>
-          <Route path = "/" element={<Payment/>} />
-          <Route path = "/paymentsuccess" element={<PaymentSuccess/>} />
-          <Route path = "/submitform" element={<Form/>} />
-          <Route path = "/mysubmission" element={<MySubmission/>} />
+          <Route path = "/login" element={<Login/>} />
+          <Route path = "/signup" element={<Signup/>}/>
+          <Route path = "/" element={<Payment/>}/>
+          <Route path = "/paymentsuccess" element={<PaymentSuccess/>}/>
+          <Route path = "/submitform" element={<Form/>}/>
+          <Route path = "/mysubmission" element={<MySubmission/>}/>
         </Routes>
       </Router>
     </>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createContext } from 'react';
+export const server="https://iiita-hub-backend-l7tm.onrender.com/api/v1";
+export const Context = createContext({isAuthenticated : false});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
