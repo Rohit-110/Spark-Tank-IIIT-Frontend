a877d4d3e146d66668fef330f96e1f742d8ab3da
rohit-110 <iit2022019@iiita.ac.in>
2025-05-25 21:56:49 +0530

    updates done

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import { createContext } from 'react';
 import { useState } from 'react';
 
 // Production API
-// export const server="https://spark-tank-iiit-backend.onrender.com/api";
+export const server="https://spark-tank-iiit-backend.onrender.com/api";
 
 // Development API
-export const server="http://localhost:4000/api";      
+// export const server="http://localhost:4000/api";      
 
 export const Context = createContext();
 
