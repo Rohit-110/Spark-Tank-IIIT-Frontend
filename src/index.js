9622b0fc663b919550420dc15a96fea9a776851c
Rohit Pandey <120464365+Rohit-110@users.noreply.github.com>
2025-08-18 02:11:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { createContext } from 'react';
 import { useState } from 'react';
 
 // Production API
-export const server="http://13.127.209.14:4000/api";
+export const server="https://spark-tank-iiit-backend.onrender.com/api";
 
 // Development API
 // export const server="http://localhost:4000/api";      
